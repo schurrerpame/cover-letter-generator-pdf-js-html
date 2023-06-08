59dab25b53ccece85ba7c33e1076bbdfe37cef07
pame <schurrerpame@gmail.com>
2023-06-08 14:46:17 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -98,8 +98,9 @@ function seePreview() {
     <div>${contact}</div>`;
 
     textCLetter.innerHTML = `<div class="pt-2 px-5">${textLetter.replace(/\n/g, '<br>')}</div>`;
-    textTemplateFooter.innerHTML = `<div class="pt-4 mt-5 px-5">
-    <div class="border-start  border-4 border-info ps-3">Contact Me
+    textTemplateFooter.innerHTML = `<div class="py-4 my-5 px-5">
+    <div class="border-start border-4 border-info ps-3">
+    Contact Me
     <div class="fw-bold">${name} </div>
     <div class="font-italic"> ${job}</div>
     <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-open" viewBox="0 0 16 16"> <path d="M8.47 1.318a1 1 0 0 0-.94 0l-6 3.2A1 1 0 0 0 1 5.4v.817l5.75 3.45L8 8.917l1.25.75L15 6.217V5.4a1 1 0 0 0-.53-.882l-6-3.2ZM15 7.383l-4.778 2.867L15 13.117V7.383Zm-.035 6.88L8 10.082l-6.965 4.18A1 1 0 0 0 2 15h12a1 1 0 0 0 .965-.738ZM1 13.116l4.778-2.867L1 7.383v5.734ZM7.059.435a2 2 0 0 1 1.882 0l6 3.2A2 2 0 0 1 16 5.4V14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V5.4a2 2 0 0 1 1.059-1.765l6-3.2Z"/> </svg> ${email} </div>
@@ -128,10 +129,10 @@ function generatePDF(e) {
         return
     }
     var element = document.getElementById('preview');
-
+  const allName= dataObj.name.trim();
     var options = {
         margin: [0, 0, 0, 0], 
-        filename: 'archivo.pdf',
+        filename: `cover-letter-${name}.pdf`,
         html2canvas: { scale: 2 },
         jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
         enableLinks: true,
